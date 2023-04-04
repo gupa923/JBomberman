7e6597233655ac7960bb72be38d4b66ad5b08729
gupa923 <gupa923@gmail.com>
2023-04-04 21:56:10 +0200

    prova branch

--- src/Gioco/JBomberman.java
+++ src/Gioco/JBomberman.java
@@ -3,6 +3,8 @@ package Gioco;
 public class JBomberman
 {
     public static void main(String[] args) {
+
         System.out.println("hello");
+        System.out.println("prova utilizzo branch");
     }
 }
