dcd115cdcf717fc68a4c059a3c0f87f737b0c6e9
gupa923 <gupa923@gmail.com>
2023-04-07 11:25:58 +0200

    ho disegnato una sottoimagine e ho messo un po' di javadoc, che dovrà poi essere scritto meglio e in inglese

--- src/Gioco/GameWindow.java
+++ src/Gioco/GameWindow.java
@@ -2,17 +2,36 @@ package Gioco;
 
 import javax.swing.*;
 
-public class GameWindow {
-    private JFrame jFrame;
+/**
+ * questa classe gestisce la finestra del gioco
+ *
+ *
+ * @autor gupa923
+ */
+public class GameWindow  extends JFrame{
 
+    /**
+     * questo costruttore crea la finistra.
+     * ha come argomento un istanza di GamePanel.
+     * in primo luogo chiama il costruttore della classe JFrame
+     * con il secondo comando faccio in modo che quando chiudo la finestra si interrompa anche l'esecuzione del programma
+     * con il metodo add aggiungo il gamePanel al JFrame
+     * con il metodo setLocation imposto la posizione in cui la finestra deve apparire
+     *con il metodo setResizeble impedisco che l'utente cambi le dimensioni della finestra durante l'esecuzione del programma
+     *
+     * con il metodo pack()faccio in modo che la dimensione del JFrame si adatti a quella dei componenti al suo interno in questo caso il gamePanel
+     *
+     * infine con setVisible faccio apparire la finestra
+     * @param gamePanel
+     */
     public GameWindow(GamePanel gamePanel){
-        jFrame = new JFrame("JBomberman");
+        super("JBomberman");
 
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jFrame.add(gamePanel);
-        jFrame.setLocation(0, 0);
-        jFrame.setResizable(false);
-        jFrame.pack();
-        jFrame.setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(gamePanel);
+        setLocation(0, 0);
+        setResizable(false);
+        pack();
+        setVisible(true);
     }
 }
--- src/Gioco/JBomberman.java
+++ src/Gioco/JBomberman.java
@@ -1,5 +1,12 @@
 package Gioco;
 
+/**
+ * in questa classe ci deve essere il metodo main e verra creato una nuova istanza di Game, che gestisce il gioco
+ *
+ *
+ * @author gupa923
+ */
+
 public class JBomberman
 {
     public static void main(String[] args) {
