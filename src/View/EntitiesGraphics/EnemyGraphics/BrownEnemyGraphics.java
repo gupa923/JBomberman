4bb904df97508254cdb6e6c0c3acee1c9f76c004
gupa923 <gupa923@gmail.com>
2024-01-05 10:39:30 +0100

    nemico marrone sistemato

--- src/View/EntitiesGraphics/EnemyGraphics/BrownEnemyGraphics.java
+++ src/View/EntitiesGraphics/EnemyGraphics/BrownEnemyGraphics.java
@@ -23,44 +23,28 @@ public class BrownEnemyGraphics extends EnemyGraphics{
         BufferedImage temp4 = loadImg("/Imgs/entitySprites/enemySprite/brown_enemy/Nemico_Marrone_DX.png");
 
         BufferedImage[] down = new BufferedImage[9];
-        down[0] = temp.getSubimage(0, 0, 10, 22);
-        down[1] = temp.getSubimage(10, 0, 10, 22);
-        down[2] = temp.getSubimage(21, 0, 10, 22);
-        down[3] = temp.getSubimage(32, 0, 14, 22);
-        down[4] = temp.getSubimage(47, 0, 16, 22);
-        down[5] = temp.getSubimage(63, 0, 16, 22);
-        down[6] = temp.getSubimage(79, 0, 16, 22);
-        down[7] = temp.getSubimage(94, 0, 14, 22);
-        down[8] = temp.getSubimage(106, 0, 12, 22);
+        for (int i = 0; i < 9; i++){
+            down[i] = temp.getSubimage(i*16, 0, 16, 22);
+
+        }
 
         BufferedImage[] dx = new BufferedImage[7];
-        dx[0] = temp4.getSubimage(0, 0, 10, 22);
-        dx[1] = temp4.getSubimage(11, 0, 10, 22);
-        dx[2] = temp4.getSubimage(21, 0, 10, 22);
-        dx[3] = temp4.getSubimage(32, 0, 11, 22);
-        dx[4] = temp4.getSubimage(44, 0, 11, 22);
-        dx[5] = temp4.getSubimage(56, 0, 11, 22);
-        dx[6] = temp4.getSubimage(66, 0, 10, 22);
+        for (int i = 0; i < 7; i++){
+            dx[i] = temp4.getSubimage(i*16, 0, 16, 22);
+
+        }
 
         BufferedImage[] sx = new BufferedImage[7];
-        sx[0] = temp3.getSubimage(0, 0, 11, 22);
-        sx[1] = temp3.getSubimage(11, 0, 11, 22);
-        sx[2] = temp3.getSubimage(22, 0, 11, 22);
-        sx[3] = temp3.getSubimage(34, 0, 11, 22);
-        sx[4] = temp3.getSubimage(45, 0, 11, 22);
-        sx[5] = temp3.getSubimage(56, 0, 11, 22);
-        sx[6] = temp3.getSubimage(66, 0, 11, 22);
+        for (int i = 0; i < 7; i++){
+            sx[i] = temp3.getSubimage(i*16, 0, 16, 22);
+
+        }
 
         BufferedImage[] up = new BufferedImage[9];
-        up[0] = temp2.getSubimage(0, 0, 10, 22);
-        up[1] = temp2.getSubimage(10, 0, 10, 22);
-        up[2] = temp2.getSubimage(21, 0, 10, 22);
-        up[3] = temp2.getSubimage(32, 0, 14, 22);
-        up[4] = temp2.getSubimage(47, 0, 16, 22);
-        up[5] = temp2.getSubimage(63, 0, 16, 22);
-        up[6] = temp2.getSubimage(79, 0, 16, 22);
-        up[7] = temp2.getSubimage(94, 0, 14, 22);
-        up[8] = temp2.getSubimage(106, 0, 12, 22);
+        for (int i = 0; i < 9; i++){
+            up[i] = temp2.getSubimage(i*16, 0, 16, 22);
+
+        }
 
         sprites = new BufferedImage[][] {down, up, sx, dx};
     }
